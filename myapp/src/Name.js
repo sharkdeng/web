210168bec5622108bb343fec69ee4d205fccd6f9
Limin <u6849956@anu.edu.au>
2019-11-01 18:30:55 +1100

    add react-dom-sample

---
+++ myapp/src/Name.js
@@ -0,0 +1,16 @@
+import React, {Component} from "react"
+
+
+const Name = () => {
+    return (
+        <div>Name Name</div>
+    )
+}
+// class Name extends Component {
+//     render() {
+//         return <div>Name Name</div>
+//     }
+// }
+
+
+export default Name;
